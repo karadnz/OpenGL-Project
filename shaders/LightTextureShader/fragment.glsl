70b7cc8bfbde64d622fb6e6f30d85ead2f9990ac
Mustafa Karadeniz <mkaradeniz2003@gmail.com>
2025-01-01 17:06:46 +0300

    fixed lighting

--- shaders/LightTextureShader/fragment.glsl
+++ shaders/LightTextureShader/fragment.glsl
@@ -4,7 +4,7 @@ out vec4 fragColor;
 in vec2 TexCoord;
 in float Factor;
 uniform sampler2D mySampler;
-uniform vec4 uDirectionalLightColor = vec4(1.0f,0.0,0.0f,1.0f);
+uniform vec4 uDirectionalLightColor = vec4(1.0f,1.0f,1.0f,1.0f);
 void main()                                            
 {                                                      
    fragColor = texture( mySampler,TexCoord)*uDirectionalLightColor*Factor;                                  
